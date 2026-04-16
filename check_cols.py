dacb3f802b9a9ede93e6bb06ceb21502d2ce1234
surprised-potato <89435318+surprised-potato@users.noreply.github.com>
2026-04-17 03:12:46 +0800

    Update and clean up modular branch

--- check_cols.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-db = pd.read_excel('aisc-shapes-database-v160-2.xlsx', sheet_name='Database v16.0')
-cols = ['Type', 'AISC_Manual_Label', 'd', 'bf', 'b', 'tw', 'tf', 't']
-print(db[db['Type'] == 'WT'][cols].head(1))
-print(db[db['Type'] == 'L'][cols].head(1))
-print(db[db['Type'] == '2L'][cols].head(1))
